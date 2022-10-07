b036e31210c7ae426acb3b12a80bc0aae535e40a
ANGADMADHEPURA <ANGADRAJ9534@GMAIL.COM>
2022-10-07 17:13:10 +0530

    updated

--- tms/includes/header.php
+++ tms/includes/header.php
@@ -38,12 +38,6 @@
 		<div class="logo wow fadeInDown animated" data-wow-delay=".5s">
 			<a href="index.php">Tourism <span>Management System</span></a>	
 		</div>
-	
-		<div class="lock fadeInDown animated" data-wow-delay=".5s"> 
-			<li><i class="fa fa-lock"></i></li>
-            <li><div class="securetxt">SAFE &amp; SECURE </div></li>
-			<div class="clearfix"></div>
-		</div>
 		<div class="clearfix"></div>
 	</div>
 </div>
