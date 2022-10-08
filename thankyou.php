57d0c7d7a53dc795c581d2228cf93945be92900b
ANGADMADHEPURA <ANGADRAJ9534@GMAIL.COM>
2022-10-08 19:04:29 +0530

    add confirm queries

--- thankyou.php
+++ thankyou.php
@@ -38,7 +38,7 @@ include('includes/config.php');
 <!--- contact ---->
 <div class="contact">
 	<div class="container">
-	<h3> Confirmation</h3>
+	<h3> Confirmation Your Query</h3>
 		<div class="col-md-10 contact-left">
 			<div class="con-top animated wow fadeInUp animated" data-wow-duration="1200ms" data-wow-delay="500ms" style="visibility: visible; animation-duration: 1200ms; animation-delay: 500ms; animation-name: fadeInUp;">
 	
